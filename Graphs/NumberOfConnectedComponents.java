df67940397a03f8d0a1d15e3d34b5aeb31cae581
manishkumar <manish.kumar@clan.business>
2025-07-27 13:59:08 +0530

    changes

--- Graphs/NumberOfConnectedComponents.java
+++ Graphs/NumberOfConnectedComponents.java
@@ -3,6 +3,34 @@ package Graphs;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a undirected Graph consisting of V vertices numbered from 0 to V-1 and E edges.
+ * The ith edge is represented by [ai,bi], denoting a edge between vertex ai and bi.
+ * We say two vertices u and v belong to a same component if there is a path from u to v or v to u.
+ * Find the number of connected components in the graph.
+
+ * A connected component is a subgraph of a graph in which there exists a path between any two vertices,
+ * and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+ * e.g
+ * 1. Input: V=4, edges=[[0,1],[1,2]]
+ * <p>
+ * Output: 2
+ * <p>
+ * Explanation: Vertices {0,1,2} forms the first component and vertex 3 forms the second component.
+ * <p>
+ * 2.  Input: V = 7, edges = [[0, 1], [1, 2], [2, 3], [4, 5]]
+ * <p>
+ * Output: 3
+ * <p>
+ * Explanation:
+ * <p>
+ * The edges [0, 1], [1, 2], [2, 3] form a connected component with vertices {0, 1, 2, 3}.
+ * <p>
+ * The edge [4, 5] forms another connected component with vertices {4, 5}.
+ * <p>
+ * Therefore, the graph has 3 connected components: {0, 1, 2, 3}, {4, 5}, and the isolated vertices {6} (vertices 6 and any other unconnected vertices).
+ */
+
 public class NumberOfConnectedComponents {
     public int findNumberOfComponent(int V, List<List<Integer>> edges) {
         int numberOfComponent = 0;
