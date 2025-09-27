3fd1dee321a8b5ddc9ee7453643ae29cce9cda2a
manishkumar <manish.kumar@clan.business>
2025-09-27 20:44:35 +0530

    changes

---
+++ DP/SubSetSum_02.java
@@ -0,0 +1,12 @@
+package DP;
+
+/**
+ * @author manishkumar
+ * https://www.youtube.com/watch?v=_gPcYovP7wc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=7
+ * <p>
+ * https://www.geeksforgeeks.org/dsa/subset-sum-problem-dp-25/
+ */
+public class SubSetSum_02 {
+
+
+}
--- DP/ZeroOneKnapsack_01.java
+++ DP/ZeroOneKnapsack_01.java
@@ -1,7 +1,7 @@
 package DP;
 
 /**
- * @author manish
+ * @author manishkumar
  * https://www.youtube.com/watch?v=ntCGbPMeqgg&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=5
  *
  * https://www.geeksforgeeks.org/dsa/0-1-knapsack-problem-dp-10/
@@ -9,24 +9,23 @@ package DP;
 public class ZeroOneKnapsack_01 {
     static int knapsack(int W, int[] val, int[] wt) {
         int n = val.length;
-        int[][] t = new int[n + 1][W + 1];
+        int[][] mem = new int[n + 1][W + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= W; j++) {
-                t[i][j] = 0;
+                mem[i][j] = 0;
             }
         }
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= W; j++) {
                 if (wt[i - 1] <= j) {
-                    t[i][j] = Math.max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
-                } else {
-                    t[i][j] = t[i - 1][j];
+                    mem[i][j] = Math.max(val[i - 1] + mem[i - 1][j - wt[i - 1]], mem[i - 1][j]);
+                }
+                else {
+                    mem[i][j] = mem[i - 1][j];
                 }
             }
-
         }
-        return t[n][W];
+        return mem[n][W];
     }
 
     public static void main(String[] args) {
