9b4a917b402f55949fc3c8ffd03b1335e7ad4125
manishkumar <manish.kumar@clan.business>
2025-11-09 20:02:12 +0530

    changes

---
+++ DP/LongestRepeatingSubsequence_20.java
@@ -0,0 +1,51 @@
+package DP;
+
+/**
+ * @author manishkumar
+ * <br>
+ * <a href="https://www.youtube.com/watch?v=hbTaCmQGqLg&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=29">YouTube: LongestRepeatingSubsequence</a>
+ * <p>
+ * <a href="https://www.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1">GFG: LongestRepeatingSubsequence</a>
+ * <p>
+ */
+public class LongestRepeatingSubsequence_20 {
+    public int LongestRepeatingSubsequence(String s) {
+        // code here
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        String s1 = s;
+        String s2 = s;
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        return modifiedLCS(s1, s2, m, n, dp);
+    }
+
+    private int modifiedLCS(String s1, String s2, int m, int n, int[][] dp) {
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1) && i != j) { // Here is the tweak
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s = "axxzxy";
+        System.out.println(new LongestRepeatingSubsequence_20().LongestRepeatingSubsequence(s));
+    }
+}
