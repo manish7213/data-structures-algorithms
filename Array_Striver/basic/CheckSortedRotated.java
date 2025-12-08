006618d3435f9569dd9ae9db4b1960b9d24b9695
manishkumar <manish.kumar@clan.business>
2025-12-08 17:29:30 +0530

    changes

---
+++ Array_Striver/basic/CheckSortedRotated.java
@@ -0,0 +1,29 @@
+package Array_Striver.basic;
+
+import java.util.Arrays;
+
+public class CheckSortedRotated {
+
+    private static boolean isSortedRotated(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {3, 4, 5, 1, 2};
+        int[] arr2 = {2, 1, 3, 4};
+        int[] arr3 = {1, 2, 3};
+//        System.out.println(isSortedRotated(arr1));
+        System.out.println(isSortedRotated(arr2));
+//        System.out.println(isSortedRotated(arr3));
+    }
+}
---
+++ DP/Pattern_05_DP_On_Trees/Q28_Max_PathSum_BinaryTree_LeafToLeaf.java
@@ -0,0 +1,17 @@
+package DP.Pattern_05_DP_On_Trees;
+
+/**
+ * @author manishkumar
+ * <br>
+ * <a href="https://www.youtube.com/watch?v=ArNyupe-XH0&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=49">YouTube: Max_PathSum_BinaryTree_LeafToLeaf</a>
+ * <p>
+ * <a href="https://www.geeksforgeeks.org/problems/maximum-path-sum/1">GFG: Max_PathSum_BinaryTree_LeafToLeaf</a>
+ * <p>
+ * <p>
+ */
+
+//TODO : COMPLETE IT LATER
+public class Q28_Max_PathSum_BinaryTree_LeafToLeaf {
+
+
+}
