8acad3c573e9ab95dbe0fa72ff43f4c3bff2c09a
manishkumar <manish.kumar@clan.business>
2025-12-10 22:50:08 +0530

    changes

---
+++ Array_Striver/basic/RotateArrayToTheLeft.java
@@ -0,0 +1,26 @@
+package Array_Striver.basic;
+
+import java.util.Arrays;
+
+public class RotateArrayToTheLeft {
+
+    public static void rotateLeft(int[] nums, int k) {
+        int n = nums.length;
+        int[] copy = nums.clone();
+        for (int i = 0; i < n; i++) {
+            nums[(i + (n - k)) % n] = copy[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+//        int[] arr = {-1,-100,3,99};
+//        int k = 2;
+        Arrays.stream(arr).forEach((element) -> System.out.print(element + " "));
+        System.out.println();
+        rotateLeft(arr, k);
+        Arrays.stream(arr).forEach((element) -> System.out.print(element + " "));
+
+    }
+}
---
+++ Array_Striver/easy/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package Array_Striver.easy;
+
+/**
+ * @author manishkumar
+ *
+ * <a href="https://leetcode.com/problems/max-consecutive-ones/description/">...</a>
+ */
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
