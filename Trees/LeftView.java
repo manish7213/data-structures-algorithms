4d956d7c47ee643da93abd25ee7590b3798d4d61
manishkumar <manish.kumar@clan.business>
2025-12-20 18:25:42 +0530

    changes

---
+++ Trees/LeftView.java
@@ -0,0 +1,50 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author manishkumar
+ *
+ * <a href="https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1">LeftView</a>
+ */
+public class LeftView {
+    public ArrayList<Integer> leftView(TreeNode root) {
+        // code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        bfs(root, res);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (ArrayList<Integer> l : res) {
+            list.add(l.get(0));
+        }
+        return list;
+    }
+
+    private void bfs(TreeNode node, ArrayList<ArrayList<Integer>> res) {
+        if (node == null) {
+            return;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(node);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                level.add(temp.val);
+                if (temp.left != null) {
+                    q.add(temp.left);
+                }
+
+                if (temp.right != null) {
+                    q.add(temp.right);
+                }
+            }
+            res.add(level);
+        }
+    }
+}
---
+++ Trees/ZigzagLevelOrderTraversal.java
@@ -0,0 +1,54 @@
+package Trees;
+
+import java.util.*;
+
+/**
+ * @author manishkumar
+ *
+ * <a href="https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/">ZigzagLevelOrderTraversal</a>
+ */
+public class ZigzagLevelOrderTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        bfs(root, res);
+        return res;
+    }
+
+    private void bfs(TreeNode root, List<List<Integer>> res) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        boolean flag = true;
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                list.add(temp.val);
+                if (temp.left != null) {
+                    q.add(temp.left);
+                }
+
+                if (temp.right != null) {
+                    q.add(temp.right);
+                }
+
+            }
+            if (!flag) {
+                Collections.reverse(list);
+
+            }
+            res.add(list);
+            flag = !flag;
+        }
+    }
+
+}
