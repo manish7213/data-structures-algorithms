a18d87315014a697909097642d0e0f12a8fdf11e
manishkumar <manish.kumar@clan.business>
2025-12-20 17:36:48 +0530

    changes

---
+++ Trees/SameTree.java
@@ -0,0 +1,24 @@
+package Trees;
+
+/**
+ * @author manishkumar
+ *
+ * <a href="https://leetcode.com/problems/same-tree/description/">SameTree</a>
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
