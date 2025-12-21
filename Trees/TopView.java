0141571f265ab1750937921d5259486c92f58850
manishkumar <manish.kumar@clan.business>
2025-12-21 17:14:43 +0530

    changes

---
+++ Trees/TopView.java
@@ -0,0 +1,51 @@
+package Trees;
+
+import java.util.*;
+
+/**
+ * @author manishkumar
+ *
+ * <a href="https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1">TopView</a>
+ */
+public class TopView {
+
+    public List<Integer> topView(TreeNode root) {
+        // code here
+
+        Map<Integer, Integer> map = new TreeMap<>();
+        bfs(root, map);
+        // System.out.println(map);
+        return new ArrayList<>(map.values());
+    }
+
+    private void bfs(TreeNode root, Map<Integer, Integer> map) {
+        if (root == null) {
+            return;
+        }
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 0));
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Pair p1 = q.poll();
+                map.putIfAbsent(p1.pos, p1.node.val);
+                if (p1.node.left != null) {
+                    q.add(new Pair(p1.node.left, p1.pos - 1));
+                }
+                if (p1.node.right != null) {
+                    q.add(new Pair(p1.node.right, p1.pos + 1));
+                }
+            }
+        }
+    }
+
+    static class Pair {
+        TreeNode node;
+        int pos;
+
+        Pair(TreeNode node, int pos) {
+            this.node = node;
+            this.pos = pos;
+        }
+    }
+}
